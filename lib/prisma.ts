fc7e58d4e8971c2f9d3dd59a2ad706a6cef5aeb6
Don97MHD <alimohammad24496@gmail.com>
2026-03-09 20:06:59 +0300

    fix: force dynamic for api to pass vercel 4

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,5 +1,5 @@
 
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '../prisma/generated/client' 
 import { Pool } from 'pg'
 import { PrismaPg } from '@prisma/adapter-pg'
 
